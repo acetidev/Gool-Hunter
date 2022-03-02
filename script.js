a719a510644ada520712049711c63376a614bff4
Matheus Aceti <acetidev@gmail.com>
2022-03-02 12:26:30 -0300

    Site finalizado

--- script.js
+++ script.js
@@ -44,3 +44,26 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+
+
+
+// carrossel
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+  idx++;
+
+  if(idx >img.length -1){
+    idx = 0;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 300}px)`;
+  
+}
+
+setInterval(carrossel, 3000);
